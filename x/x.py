fc6fa97f58a7088c1ce8051c313bcb859116fabf
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-10-23 00:08:33 -0400

    Add debugging CLA options

--- x/x.py
+++ x/x.py
@@ -10,6 +10,9 @@ from .compiler.compiler import Compiler
 def entry():
     parser = argparse.ArgumentParser(description='X compiler')
     parser.add_argument('-i', '--input', help='X code file', type=str)
+    parser.add_argument('-t', '--tokens', action='store_true', help='Print tokens')
+    parser.add_argument('-p', '--parse', action='store_true', help='Print parse tree')
+    parser.add_argument('-c', '--compile', action='store_true', help='Print opcodes')
     args = parser.parse_args()
 
     if not os.path.exists(args.input):
@@ -25,11 +28,18 @@ def entry():
         source = f.read()
 
     tokens = Tokenizer(source).generate_tokens()
-    ast_root = RecursiveDescentParser(tokens=tokens).parse()
-    tree_walker = TreeWalker(root=ast_root)
-    tree_walker.walk_and_print()
-    compiler = Compiler(ast_root=ast_root)
-    opcodes = compiler.compile()
 
-    for opcode in opcodes:
-        print(opcode)
+    if args.tokens:
+        for token in tokens:
+            print(token)
+
+    ast_root = RecursiveDescentParser(tokens=tokens).parse()
+
+    if args.parse:
+        TreeWalker(root=ast_root).walk_and_print()
+
+    opcodes = Compiler(ast_root=ast_root).compile()
+
+    if args.compile:
+        for opcode in opcodes:
+            print(opcode)
