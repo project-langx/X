eded99bb9d4925544468a9114bc84ca4c1eca831
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-11-07 03:02:02 -0500

    Add support for new grammar in stack based C++ decompiler

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World" << std::endl;
+	std::cout << 3.14 << std::endl;
+	std::cout << 1 + 2 * 3;; << std::endl;
+	std::cout << 4 - 8 / 2;; << std::endl;
+	std::cout << 4.0 - 8.0 / 2.0;; << std::endl;
+
+	return 0; 
+}
