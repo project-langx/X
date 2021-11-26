60dc1d423bdfdb92601d18c7b358dcb41f906057
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-11-26 02:45:23 -0500

    Fix token test for TokenType enum

--- langx/test/tokenizer_test/test_token.py
+++ langx/test/tokenizer_test/test_token.py
@@ -1,13 +1,14 @@
 import unittest
 
 from ...tokenizer.token import Token
+from ...tokenizer.token_type import TokenType
 
 
 class TestToken(unittest.TestCase):
-    def test_create_token(self):
-        token: Token = Token(type="__number__", dtype="int", value="3", line_num=1)
+    def test_create_token(self) -> None:
+        token: Token = Token(type=TokenType.NUMBER, dtype="int", value="3", line_num=1)
 
-        self.assertEqual(token.type, "__number__")
+        self.assertEqual(token.type, TokenType.NUMBER)
         self.assertEqual(token.dtype, "int")
         self.assertEqual(token.value, "3")
         self.assertEqual(token.line_num, 1)
