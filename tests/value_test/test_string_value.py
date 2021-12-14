8c5bc206648e7b4a9db441f1b7d3b446e2b69829
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2021-12-14 02:08:41 -0500

    Add tests for string value

--- tests/value_test/test_string_value.py
+++ tests/value_test/test_string_value.py
@@ -17,3 +17,29 @@ class TestStringValue(unittest.TestCase):
     def test_get_value(self) -> None:
         string_value: StringValue = StringValue(value="hello")
         self.assertEqual(string_value.get_value(), "hello")
+
+    @typing.no_type_check
+    def test_add_null_other(self) -> None:
+        string_value: StringValue = StringValue(value="hello")
+        with self.assertRaises(AssertionError):
+            string_value.add(o=None)
+
+    def test_add(self) -> None:
+        string_value: StringValue = StringValue(value="hello")
+        self.assertEqual(string_value.add(o=StringValue(value="world")).get_value(), "helloworld")
+
+    def test_sub(self) -> None:
+        with self.assertRaises(NotImplementedError):
+            StringValue(value="hello").sub(o=StringValue(value="world"))
+
+    def test_mul(self) -> None:
+        with self.assertRaises(NotImplementedError):
+            StringValue(value="hello").mul(o=StringValue(value="world"))
+
+    def test_truediv(self) -> None:
+        with self.assertRaises(NotImplementedError):
+            StringValue(value="hello").truediv(o=StringValue(value="world"))
+
+    def test_floordiv(self) -> None:
+        with self.assertRaises(NotImplementedError):
+            StringValue(value="hello").floordiv(o=StringValue(value="world"))
